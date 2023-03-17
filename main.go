a9a69976d439674c8c7de7d270a01ae310234025
Slin Lee <slin@slinlee.com>
2023-03-17 14:45:43 -0700

    Remove spinner

--- main.go
+++ main.go
@@ -5,19 +5,21 @@ import (
 	"os"
 
 	"github.com/charmbracelet/bubbles/key"
-	"github.com/charmbracelet/bubbles/spinner"
+	// "github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	// "github.com/charmbracelet/lipgloss"
 )
 
 type errMsg error
 
 type model struct {
-	spinner  spinner.Model
-	quitting bool
-	err      error
-	x int
-	y int
+	// spinner      spinner.Model
+	quitting     bool
+	err          error
+	x            int
+	y            int
+	canvasWidth  int
+	canvasHeight int
 }
 
 var quitKeys = key.NewBinding(
@@ -26,14 +28,16 @@ var quitKeys = key.NewBinding(
 )
 
 func initialModel() model {
-	s := spinner.New()
-	s.Spinner = spinner.Dot
-	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
-	return model{spinner: s}
+	// s := spinner.New()
+	// s.Spinner = spinner.Dot
+	// s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	// return model{spinner: s}
+	return model{}
 }
 
 func (m model) Init() tea.Cmd {
-	return m.spinner.Tick
+	// return m.spinner.Tick
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -48,20 +52,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, nil
 	case tea.MouseMsg:
 		switch msg.Type {
-				case tea.MouseLeft:
-				case tea.MouseMotion:
-					m.x = msg.X
-					m.y = msg.Y
+		case tea.MouseMotion:
+			m.x = msg.X
+			m.y = msg.Y
 
-					return m, nil
-			}
+			return m, nil
+		}
+	case tea.WindowSizeMsg:
+		// set window size for calculations
+		m.canvasWidth, m.canvasHeight = msg.Width, msg.Height
+		return m, nil
 	case errMsg:
 		m.err = msg
 		return m, nil
 
 	default:
 		var cmd tea.Cmd
-		m.spinner, cmd = m.spinner.Update(msg)
+		// m.spinner, cmd = m.spinner.Update(msg)
 		return m, cmd
 	}
 	return m, nil
@@ -71,10 +78,12 @@ func (m model) View() string {
 	if m.err != nil {
 		return m.err.Error()
 	}
-	str := fmt.Sprintf("\n\n   %s Loading forever...%d %d %s\n\n",  
-		m.spinner.View(), 
-		m.x, 
+	str := fmt.Sprintf("\n\nMouse: %d %d\nWindow: %d %d \n %s\n\n",
+		// m.spinner.View(),
+		m.x,
 		m.y,
+		m.canvasWidth,
+		m.canvasHeight,
 		quitKeys.Help().Desc)
 	if m.quitting {
 		return str + "\n"
@@ -84,7 +93,7 @@ func (m model) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(initialModel(), 
+	p := tea.NewProgram(initialModel(),
 		tea.WithAltScreen(),
 		tea.WithMouseAllMotion())
 	if _, err := p.Run(); err != nil {
